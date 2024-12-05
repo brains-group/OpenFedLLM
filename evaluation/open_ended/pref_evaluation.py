897c0cff3469c23dd0594e519d476f88c59082c4
fespadea <fespadearocks@gmail.com>
2024-12-05 02:52:30 -0500

    finished

--- evaluation/open_ended/pref_evaluation.py
+++ evaluation/open_ended/pref_evaluation.py
@@ -44,73 +44,73 @@ if last_str.startswith("full"):  # if the model is merged as full model
 else:
     kto_dist_model_name = last_str
 
-# advbench dpo
-sys.argv = ["", "--base_model_path", dpo_model_path, "--bench_name", "advbench"]
-runpy.run_path("./gen_model_answer.py")
+# # advbench dpo
+# sys.argv = ["", "--base_model_path", dpo_model_path, "--bench_name", "advbench"]
+# runpy.run_path("./gen_model_answer.py")
 
-sys.argv = ["", "--model_answer", dpo_model_name]
-runpy.run_path("./gen_judge_advbench.py")
+# sys.argv = ["", "--model_answer", dpo_model_name]
+# runpy.run_path("./gen_judge_advbench.py")
 
-# advbench kto
-sys.argv = ["", "--base_model_path", kto_model_path, "--bench_name", "advbench"]
-runpy.run_path("./gen_model_answer.py")
+# # advbench kto
+# sys.argv = ["", "--base_model_path", kto_model_path, "--bench_name", "advbench"]
+# runpy.run_path("./gen_model_answer.py")
 
-sys.argv = ["", "--model_answer", kto_model_name]
-runpy.run_path("./gen_judge_advbench.py")
+# sys.argv = ["", "--model_answer", kto_model_name]
+# runpy.run_path("./gen_judge_advbench.py")
 
-# advbench kto_dist
-sys.argv = ["", "--base_model_path", kto_dist_model_path, "--bench_name", "advbench"]
-runpy.run_path("./gen_model_answer.py")
+# # advbench kto_dist
+# sys.argv = ["", "--base_model_path", kto_dist_model_path, "--bench_name", "advbench"]
+# runpy.run_path("./gen_model_answer.py")
 
-sys.argv = ["", "--model_answer", kto_dist_model_name]
-runpy.run_path("./gen_judge_advbench.py")
+# sys.argv = ["", "--model_answer", kto_dist_model_name]
+# runpy.run_path("./gen_judge_advbench.py")
 
-# Vicuna
-sys.argv = ["", "--base_model_path", dpo_model_path, "--bench_name", "vicuna"]
-runpy.run_path("./gen_model_answer.py")
-sys.argv = ["", "--base_model_path", kto_model_path, "--bench_name", "vicuna"]
-runpy.run_path("./gen_model_answer.py")
-sys.argv = ["", "--base_model_path", kto_dist_model_path, "--bench_name", "vicuna"]
-runpy.run_path("./gen_model_answer.py")
+# # Vicuna
+# sys.argv = ["", "--base_model_path", dpo_model_path, "--bench_name", "vicuna"]
+# runpy.run_path("./gen_model_answer.py")
+# sys.argv = ["", "--base_model_path", kto_model_path, "--bench_name", "vicuna"]
+# runpy.run_path("./gen_model_answer.py")
+# sys.argv = ["", "--base_model_path", kto_dist_model_path, "--bench_name", "vicuna"]
+# runpy.run_path("./gen_model_answer.py")
 
-sys.argv = [
-    "",
-    "--ans1_file_path",
-    f"./data/vicuna/model_answer/{dpo_model_name}.jsonl",
-    "--ans2_file_path",
-    f"./data/vicuna/model_answer/{kto_model_name}.jsonl",
-    "--ansmore_file_paths",
-    f"./data/vicuna/model_answer/{kto_dist_model_name}.jsonl",
-]
-runpy.run_path("./JudgeLM/judgelm/data/JudgeLM/judgelm_preprocess.py", run_name="__main__")
+# sys.argv = [
+#     "",
+#     "--ans1_file_path",
+#     f"./data/vicuna/model_answer/{dpo_model_name}.jsonl",
+#     "--ans2_file_path",
+#     f"./data/vicuna/model_answer/{kto_model_name}.jsonl",
+#     "--ansmore_file_paths",
+#     f"./data/vicuna/model_answer/{kto_dist_model_name}.jsonl",
+# ]
+# runpy.run_path("./JudgeLM/judgelm/data/JudgeLM/judgelm_preprocess.py", run_name="__main__")
 
 answerFile = f"./data/vicuna/model_judgement/{dpo_model_name}+{kto_model_name}.jsonl"
-sys.argv = [
-    "",
-    "--model-path",
-    "BAAI/JudgeLM-13B-v1.0",
-    "--model-id",
-    "JudgeLM-13B-v1.0",
-    "--question-file",
-    f"./data/vicuna/model_answer/{dpo_model_name}-judgelm-val-5k-judge-samples.jsonl",
-    "--answer-file",
-    answerFile,
-    "--num-gpus-per-model",
-    "1",
-    "--num-gpus-total",
-    "1",
-    "--temperature",
-    "0.2",
-    "--if-fast-eval",
-    "0",
-    "--answer-num",
-    "3",
-]
-# runpy.run_path("./JudgeLM/judgelm/llm_judge/gen_model_judgement_multi.py")
-subprocess.run(
-    f'conda run --no-capture-output -n judgelm python ./JudgeLM/judgelm/llm_judge/gen_model_judgement_multi.py {" ".join(sys.argv)}',
-    shell=True,
-)
+# sys.argv = [
+#     "",
+#     "--model-path",
+#     "BAAI/JudgeLM-13B-v1.0",
+#     "--model-id",
+#     "JudgeLM-13B-v1.0",
+#     "--question-file",
+#     f"./data/vicuna/model_answer/{dpo_model_name}-judgelm-val-5k-judge-samples.jsonl",
+#     "--answer-file",
+#     answerFile,
+#     "--num-gpus-per-model",
+#     "1",
+#     "--num-gpus-total",
+#     "1",
+#     "--temperature",
+#     "0.2",
+#     "--if-fast-eval",
+#     "0",
+#     "--answer-num",
+#     "3",
+# ]
+# # runpy.run_path("./JudgeLM/judgelm/llm_judge/gen_model_judgement_multi.py")
+# subprocess.run(
+#     f'conda run --no-capture-output -n judgelm python ./JudgeLM/judgelm/llm_judge/gen_model_judgement_multi.py {" ".join(sys.argv)}',
+#     shell=True,
+# )
 
 totalDPOScore = 0
 totalKTOScore = 0
@@ -120,9 +120,9 @@ with open(answerFile, "r") as ques_file:
     for line in ques_file:
         if line:
             judgement = json.loads(line)["pred_text"]
-            if not re.search(r"^\d+[.]?\d*", judgement):
-                continue
             scores = re.findall(r"\d+[.]?\d*", judgement)
+            if not re.search(r"^\d+[.]?\d*", judgement) or len(scores) < 3:
+                continue
             totalDPOScore += float(scores[0])
             totalKTOScore += float(scores[1])
             totalKTODistScore += float(scores[2])
@@ -189,9 +189,9 @@ with open(answerFile, "r") as ques_file:
     for line in ques_file:
         if line:
             judgement = json.loads(line)["pred_text"]
-            if not re.search(r"^\d+[.]?\d*", judgement):
-                continue
             scores = re.findall(r"\d+[.]?\d*", judgement)
+            if not re.search(r"^\d+[.]?\d*", judgement) or len(scores) < 3:
+                continue
             totalDPOScore += float(scores[0])
             totalKTOScore += float(scores[1])
             totalKTODistScore += float(scores[2])
